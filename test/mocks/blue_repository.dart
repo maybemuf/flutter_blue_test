eef03dc00dcd1f004a50b9079d1d032dc4964b91
Lesha <maybemuf@gmail.com>
2024-03-18 17:28:26 +0200

    added test coverage for cubits

---
+++ test/mocks/blue_repository.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_blue_test/features/blue/repositories/blue_repository.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<BlueRepository>()])
+import 'blue_repository.mocks.dart';
